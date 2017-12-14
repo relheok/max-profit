d8a4d9643c1ab3426eda4ccab07c9b13f1d916b6
koehle_j <jeremy.koehler@epitech.eu>
2017-12-14 18:26:21 +0100

    Refactor simplex to use configparser

--- simplex.py
+++ simplex.py
@@ -1,25 +1,38 @@
+"""
+Module implementing simplex algorithm
+"""
+
+import configparser
+
 class Simplex:
-    def __init__(self, resources, prices):
-        self.mtx = [[1, 0, 1, 0, 2, 1, 0, 0, 0, 0, resources[0]],
-                    [1, 2, 0, 1, 0, 0, 1, 0, 0, 0, resources[1]],
-                    [2, 1, 0, 1, 0, 0, 0, 1, 0, 0, resources[2]],
-                    [0, 0, 3, 1, 2, 0, 0, 0, 1, 0, resources[3]]]
-
-        # set matrix last line
-        pr = []
-        for i in range(len(prices)):
-            check = True
-            for x in range(len(self.mtx)):
-                check = self.mtx[x][i] == 0 or self.mtx[x][-1] != 0
-            pr.append(-prices[i] if check else prices[i])
-        pr.extend([0] * (len(prices) - 1) + [1, 0])
-        self.mtx.append(pr)
-
+    def __init__(self, resources, prices, parser):
         self.pivot = {'x': -1, 'y': -1}
         self.resources = resources
         self.prices = prices
         self.base = [-1] * len(prices)
 
+        self.mtx = []
+        self.init_mtx(parser)
+
+    def init_mtx(self, parser):
+        values = parser.get_constraints_values()
+        psize = len(self.prices)
+        for i in range(len(self.resources)):
+            self.mtx.append(values[i]
+                            + [0] * i
+                            + [1]
+                            + [0] * (psize - i - 1)
+                            + [self.resources[i]])
+
+        # set matrix last line
+        pr = []
+        for i in range(psize):
+            check = True
+            for x in range(len(self.mtx)):
+                check = self.mtx[x][i] == 0 or self.mtx[x][-1] != 0
+            pr.append(-self.prices[i] if check else self.prices[i])
+        pr.extend([0] * (psize - 1) + [1, 0])
+        self.mtx.append(pr)
 
     def solve(self):
         # We are finished when all the elements of the last lines are greate or equal to 0
@@ -32,7 +45,6 @@ class Simplex:
                 self.base[self.base.index(self.pivot['x'])] = -1
             if self.pivot['y'] < len(self.base):
                 self.base[self.pivot['y']] = self.pivot['x']
-
 
     def get_pivot_position(self):
         self.pivot = {'x': -1, 'y': -1} # reset pivot
@@ -54,7 +66,6 @@ class Simplex:
 
         return self.pivot
 
-
     def set_values(self):
         pivot_value = self.mtx[self.pivot['x']][self.pivot['y']]
 
@@ -70,10 +81,13 @@ class Simplex:
             for y in range(len(self.mtx[x])):
                 self.mtx[x][y] -= self.mtx[self.pivot['x']][y] * value
 
-
     def __str__(self):
         resources = ", ".join("{} F{}".format(x, i + 1) for i, x in enumerate(self.resources))
         products = ["{:.2f}".format(self.mtx[x][-1]) if x != -1 else 0 for x in self.base]
+        """
+        formula to get the total production value:
+        sum(float(products[i]) * self.prices[i] for i in range(len(self.prices)))
+        """
         return ("resources: {}\n\n".format(resources)
                 + "oatmeal: {} units at € {} /unit\n".format(products[0], self.prices[0])
                 + "wheat: {} units at € {} /unit\n".format(products[1], self.prices[1])
@@ -83,7 +97,6 @@ class Simplex:
                 + "total production value: € {:.2f}".format(self.mtx[-1][-1]))
 
 
-"""
-formula to get the total production value:
-sum(float(products[i]) * self.prices[i] for i in range(len(self.prices)))
-"""
+def simplex(resources, prices, csvfile=""):
+    spx = Simplex(resources, prices, configparser.configparser(csvfile))
+    return spx
