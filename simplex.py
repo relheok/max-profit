55850351c23fcc3802545ee60b95e8eb506af274
koehle_j <jeremy.koehler@epitech.eu>
2017-12-14 15:38:20 +0100

    Add simplex.getPivotPosition

--- simplex.py
+++ simplex.py
@@ -6,6 +6,7 @@ class Simplex:
                     [0, 0, 3, 1, 2, 0, 0, 0, 1, 0, resources[3]],
                     [-prices[0], -prices[1], -prices[2], -prices[3], -prices[4], 0, 0, 0, 0, 1, 0]]
 
+        self.pivot = {'x': -1, 'y': -1}
         self.resources = resources
         self.prices = prices
 
@@ -13,4 +14,27 @@ class Simplex:
     def solve(self):
         # We are finished when all the elements of the last lines are greate or equal to 0
         while any(x < 0 for x in self.mtx[-1]):
-            pass
+
+            self.getPivotPosition()
+    def getPivotPosition(self):
+        self.pivot = {'x': -1, 'y': -1} # reset pivot
+
+        mini = 0
+
+        for y in range(len(self.mtx[-1])):
+            if self.mtx[-1][y] < mini:
+                mini = self.mtx[-1][y]
+                self.pivot['y'] = y
+
+        mini = -1
+
+        for x in range(len(self.mtx)):
+            if self.mtx[x][self.pivot['y']] == 0:
+                continue
+
+            res = self.mtx[x][-1] / self.mtx[x][self.pivot['y']]
+            if res > 0 and (res < mini or mini == -1):
+                mini = res
+                self.pivot['x'] = x
+
+        return self.pivot
